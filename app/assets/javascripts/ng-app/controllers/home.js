9496f6d8ae760b29ffcd65702e363898fd91a55f
jose <bdj19@hotmail.com>
2015-01-30 17:16:39 -0500

    fix

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -9,30 +9,27 @@
 		'Ruby','OpenSCAD','Scala','SCSS','SH','SQL','SVG','Text','Textile','XML','XQuery','YAML'];
 	  $scope.mode = $scope.modes[0];
 	 
-	 
-	  // ui-ace option
-	  $scope.aceOption = {
-	    mode: $scope.mode.toLowerCase(),
-	    onLoad: function (editor) {	
-	      // defaults 
-          editor.setTheme("ace/theme/monokai");
-           // options
-           editor.setOptions({
+
+	 $scope.aceLoaded = function(_editor) {
+	 mode: $scope.mode.toLowerCase(),
+    $scope.aceSession = _editor.getSession();
+     _editor.setTheme("ace/theme/monokai");
+
+      // options
+           _editor.setOptions({
            	showGutter: true,
            	showPrintMargin: false,
-           	enableBasicAutocompletion: true,
-			enableSnippets: true,
-			enableLiveAutocompletion: true,
            	fontSize: '14px',
            	fontFamily: 'menlo'
            });
-
-	      // HACK to have the ace instance in the scope...
+           // HACK to have the ace instance in the scope...
 	      $scope.modeChanged = function () {
-	        editor.getSession().setMode("ace/mode/" + $scope.mode.toLowerCase());
+	        _editor.getSession().setMode("ace/mode/" + $scope.mode.toLowerCase());
 	      };
-	 
-	    }
-	  };
+	};
+	$scope.aceChanged = function () {
+	    $scope.aceDocumentValue = $scope.aceSession.view.contents().getValue();
+	};
+
 });
    
