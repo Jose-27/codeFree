25bb060efecc3b2b4d498eba2f02459d4a7842ef
jose <bdj19@hotmail.com>
2015-02-12 15:00:29 -0500

    fixes

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -4,16 +4,15 @@
 
    app.controller('AceCtrl', function ($scope) {
 	// select
-	  $scope.modes = ['CoffeeScript','ColdFusion','C#','CSS','Dart','Go','Groovy','haXe','HTML','C/C++','Clojure',
-		'Java','JavaScript','JSON','LaTeX','LESS','Liquid','Lua','Markdown','OCaml','Perl','pgSQL','PHP','Powershell','Python',
-		'Ruby','OpenSCAD','Scala','SCSS','SH','SQL','SVG','Text','Textile','XML','XQuery','YAML'];
-	  $scope.mode = $scope.modes[0];
+	  // $scope.modes = ['HTML'];
+	  // $scope.mode = $scope.modes[0];
 	 
 
 	$scope.aceLoaded = function(_editor) {
-     mode: $scope.mode.toLowerCase(),
+     // mode: $scope.mode.toLowerCase(),
     $scope.aceSession = _editor.getSession();
      _editor.setTheme("ace/theme/monokai");
+     _editor.getSession().setMode("ace/mode/html");
 
       // options
            _editor.setOptions({
@@ -22,10 +21,10 @@
            	fontSize: '14px',
            	fontFamily: 'menlo'
            });
-           // HACK to have the ace instance in the scope...
-	      $scope.modeChanged = function () {
-	        _editor.getSession().setMode("ace/mode/" + $scope.mode.toLowerCase());
-	      };
+       //     // HACK to have the ace instance in the scope...
+	      // $scope.modeChanged = function () {
+	      //   _editor.getSession().setMode("ace/mode/" + $scope.mode.toLowerCase());
+	      // };
     };
 
 	$scope.aceChanged = function () {
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+
+  get 'users/new'
+
   root 'application#index'
   get '*path.html' => 'application#index', :layout => 0
   get "*path" => "application#index"
